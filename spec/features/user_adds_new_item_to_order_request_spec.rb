31e46c1c2ca808d41972f3eb09b070b2a7aaf703
Tommy Hui <hui.1tommy@gmail.com>
2016-04-23 10:04:19 -0400

    fixed tests

--- spec/features/user_adds_new_item_to_order_request_spec.rb
+++ spec/features/user_adds_new_item_to_order_request_spec.rb
@@ -6,7 +6,8 @@ feature 'User adds new item to order request' do
     before(:each) do
       FactoryGirl.create(:vendor, name: 'vendor1')
       FactoryGirl.create(:vendor, name: 'vendor2')
-      FactoryGirl.create(:user, username: 'labtech', email: 'labtech@email.com', password: 'password')
+      FactoryGirl.create(:user, username: 'labtech', email: 'labtech@email.com')
+      FactoryGirl.create(:user, username: 'labtech1', email: 'labtech1@email.com')
       visit new_user_session_path
       fill_in 'Login', with: 'labtech@email.com'
       fill_in 'Password', with: 'password'
@@ -49,6 +50,30 @@ feature 'User adds new item to order request' do
       expect(page).to have_content('15.25')
       expect(page).to have_content('http://www.google.com')
     end
+
+    scenario "any user can add items to an exisiting order request as long as it's not marked as 'ordered'" do
+      click_on 'Sign Out'
+      visit new_user_session_path
+      fill_in 'Login', with: 'labtech1'
+      fill_in 'Password', with: 'password'
+      click_on 'Log in'
+      click_on 'List of Orders'
+      click_on 'vendor1'
+      fill_in 'Cat number', with: 'ABC123'
+      fill_in 'Product name', with: 'Test Product'
+      fill_in 'Quantity', with: '2'
+      fill_in 'Price', with: '15.25'
+      fill_in 'URL', with: 'http://www.google.com'
+      click_on 'Add Item'
+
+      expect(page).to have_content('Item Added!')
+      expect(page).to have_content('ABC123')
+      expect(page).to have_content('Test Product')
+      expect(page).to have_content('2')
+      expect(page).to have_content('15.25')
+      expect(page).to have_content('http://www.google.com')
+    end
+
   end
 
   scenario 'User is not logged in and tries to add a new item form' do
