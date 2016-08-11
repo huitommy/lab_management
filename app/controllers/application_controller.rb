e6fdfc8775f5f20108f55999cf62d617af61447a
Tommy Hui <hui.1tommy@gmail.com>
2016-08-10 20:53:26 -0400

    fixed hound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,9 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:username, :email, :password, :password_confirmation, :remember_me])
-    devise_parameter_sanitizer.permit(:sign_in, keys:[:login, :username, :email, :password, :remember_me])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username, :email, :password, :password_confirmation, :remember_me ])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [ :login, :username, :email, :password, :remember_me ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :username, :email, :password, :password_confirmation, :current_password ])
   end
 
   protect_from_forgery with: :exception
